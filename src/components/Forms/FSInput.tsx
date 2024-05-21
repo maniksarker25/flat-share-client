4b267dd0635611dd6adc2741113d5ef9526a61d9
Manik <tonysins43@gmail.com>
2024-05-22 02:32:58 +0600

    create reuseable input component

---
+++ src/components/Forms/FSInput.tsx
@@ -0,0 +1,45 @@
+import { SxProps, TextField } from "@mui/material";
+import { Controller, useFormContext } from "react-hook-form";
+
+type TInputProps = {
+  name: string;
+  label?: string;
+  type?: string;
+  size?: "small" | "medium";
+  fullWidth?: boolean;
+  sx?: SxProps;
+  required?: boolean;
+};
+
+const FSInput = ({
+  name,
+  label,
+  type = "text",
+  size = "small",
+  fullWidth,
+  sx,
+  required,
+}: TInputProps) => {
+  const { control } = useFormContext();
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field, fieldState: { error } }) => (
+        <TextField
+          {...field}
+          label={label}
+          type={type}
+          variant="outlined"
+          size={size}
+          required={required}
+          fullWidth={fullWidth}
+          error={!!error?.message}
+          helperText={error?.message}
+        />
+      )}
+    />
+  );
+};
+
+export default FSInput;
