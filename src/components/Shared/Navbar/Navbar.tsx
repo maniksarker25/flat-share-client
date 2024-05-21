bde43ae1fa75aaef1979bdbaf914a93bd59b0b93
Manik <tonysins43@gmail.com>
2024-05-21 23:49:20 +0600

    create responsive navbar

--- src/components/Shared/Navbar/Navbar.tsx
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -1,7 +1,105 @@
+"use client";
 import React from "react";
-
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import MenuIcon from "@mui/icons-material/Menu";
+import Box from "@mui/material/Box";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
+import { Container } from "@mui/material";
+import Link from "next/link";
+import Image from "next/image";
+import navLogo from "@/assets/images/logo/house-share-logo-icon-design-vector-22925067-removebg-preview.png";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
+  const handleMenu = (event: any) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Container>
+      <Box position="static">
+        <Toolbar>
+          <Typography component="div" sx={{ flexGrow: 1 }}>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyItems: "center",
+              }}
+            >
+              <Image src={navLogo} alt="logo" height={100} width={100} />
+              <Typography fontSize={"26px"} fontWeight={600}>
+                Flat Share
+              </Typography>
+            </Box>
+          </Typography>
+          {isMobile ? (
+            <Box>
+              <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                onClick={handleMenu}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+              >
+                <MenuItem href={"/"}>Home</MenuItem>
+                <MenuItem href={"/"}>About Us</MenuItem>
+                <MenuItem href={"/"}>My Profile</MenuItem>
+              </Menu>
+            </Box>
+          ) : (
+            <Box sx={{ display: "flex", gap: "20px", alignItems: "center" }}>
+              <Link href={"/"} color="inherit">
+                Home
+              </Link>
+              <Link href={"/"} color="inherit">
+                About Us
+              </Link>
+              <Link href={"/"} color="inherit">
+                My Profile
+              </Link>
+              <Button
+                href={"/"}
+                sx={{
+                  ml: "40px",
+                }}
+              >
+                Login
+              </Button>
+            </Box>
+          )}
+        </Toolbar>
+      </Box>
+    </Container>
+  );
 };
 
 export default Navbar;
